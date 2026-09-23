57b3b1a0cc20fd97b0a0a6a96c81054cfe9c7dd9
kdeme <7857583+kdeme@users.noreply.github.com>
2026-09-23 13:51:47 +0200

    Dual-license under Apache 2.0 and MIT
    LICENSE-APACHE beside LICENSE-MIT

--- .github/scripts/free-up-disk-space.sh
+++ .github/scripts/free-up-disk-space.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
-#
-# Nimbus
+
+# nimbus-zkvm-guests
 # Copyright (c) 2026 Status Research & Development GmbH
-# Licensed under either of
-#  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
-#    http://www.apache.org/licenses/LICENSE-2.0)
-#  * MIT license ([LICENSE-MIT](LICENSE-MIT) or
-#    http://opensource.org/licenses/MIT)
-# at your option. This file may not be copied, modified, or distributed except
-# according to those terms.
-#
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
+
 # The ZisK proving key needs ~22 GB at peak: a 3.7 GB download, 14 GB unpacked,
 # and ~3.6 GB of program-setup output. GitHub documents 14 GB of storage for a
 # standard Linux runner, and the image arrives with a good deal of it spent, so
--- scripts/run-test-fixtures.sh
+++ scripts/run-test-fixtures.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
-# Nimbus
+# nimbus-zkvm-guests
 # Copyright (c) 2026 Status Research & Development GmbH
-# Licensed under either of
-#  * Apache License, version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
-#    http://www.apache.org/licenses/LICENSE-2.0)
-#  * MIT license ([LICENSE-MIT](LICENSE-MIT) or
-#    http://opensource.org/licenses/MIT)
-# at your option. This file may not be copied, modified, or distributed except
-# according to those terms.
-#
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at https://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at https://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
+
 # Feeds each <name>.input.bin to the guest and requires <name>.output.bin back.
 #
 # usage: run-test-fixtures.sh <emulator> <elf> <timeout-seconds> <name.input.bin>...
